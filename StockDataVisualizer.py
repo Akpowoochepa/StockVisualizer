16ba60fa936abb022e3d7b8778537dc4ae530818
Akpowo ochepa <manny.babida@gmail.com>
2023-03-16 22:33:29 -0500

    StockDataVisualizer with Time/Date commited

--- StockDataVisualizer.py
+++ StockDataVisualizer.py
@@ -7,4 +7,13 @@ def main():
     print("\n\nChart Types\n--------------\n1. Bar\n2. Line\n")
     chart_type = get_chart_type() #function to get chart type from user
 
+# Call the function to get the time series option from the user
+time_series_option = get_time_series_option()
+
+# Call the function to get the start date from the user
+start_date = get_start_date()
+
+# Call the function to get the end date from the user
+end_date = get_end_date(start_date)
+
 main()
