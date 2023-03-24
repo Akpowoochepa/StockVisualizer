dbecf0ea1014792d95956b0db57c3a2a2d235903
donovanhargett <donovanhargett@gmail.com>
2023-03-24 16:44:52 -0500

    Updated graphing functionality

--- StockDataVisualizer.py
+++ StockDataVisualizer.py
@@ -3,6 +3,8 @@ import pygal
 from lxml import etree, html
 from StockFunctions import *
 from StockTime import *
+from ping import *
+import pandas
 
 def main():
     print("Stock Data Visualizer\n------------------------\n")
@@ -38,86 +40,84 @@ def main():
 
     # Call the function to get the end date from the user
     end_date = get_end_date(start_date)
+    
+    
+    def DictionaryIterator(dict_obj):
+        stack = [(k, v) for k, v in dict_obj.items()]
+        while stack:
+            k, v = stack.pop()
+            if isinstance(v, dict):
+                stack.extend([(k + '.' + k2, v2) for k2, v2 in v.items()])
+            else:
+                yield (k, v)
 
 
+    def dataSeperator(valueKey):
+        result = []
+        for dictValues in DictionaryIterator(data):
+            if dictValues[1] in inputRange and dictValues[2] == valueKey:
+                result.append(eval(dictValues[3]))
+        return list(reversed(result))
 
-    # STOCK VISUALIZATION
-    # Get stock data from API
-    api_key = 'AFWROUKTO2W1ZDY'
-    url = f'https://www.alphavantage.co/query?function={time_series}&symbol={stock_symbol}&interval={interval}&apikey={api_key}'
-    response = requests.get(url)
-    data = response.json()
+    def dateChecker():
+        unique_values = set()
+        result = []
+        for dictValues in DictionaryIterator(data):
+            value = dictValues[1]
+            if value in inputRange and value not in unique_values:
+                unique_values.add(value)
+                result.append(value)
+        return result[::-1]
 
-    # Extract data for line chart
-    line_dates = []
-    line_prices = []
-    line_open_prices = []
-    line_high_prices = []
-    line_low_prices = []
-    line_close_prices = []
-    for date, prices in data[f'Time Series ({interval or "daily"})'].items():
-        line_dates.append(date)
-        line_prices.append(float(prices['4. close']))
-        line_open_prices.append(float(prices['1. open']))
-        line_high_prices.append(float(prices['2. high']))
-        line_low_prices.append(float(prices['3. low']))
-        line_close_prices.append(float(prices['4. close']))
-
-    # Extract data for bar chart
-    bar_dates = []
-    bar_volumes = []
-    for date, volumes in data[f'Time Series ({interval or "daily"})'].items():
-        bar_dates.append(date)
-        bar_volumes.append(int(volumes['5. volume']))
-
-    # Create line chart
-    line_chart = pygal.Line(x_label_rotation=20)
-    line_chart.title = f'{stock_symbol} Stock Prices'
-    line_chart.x_labels = line_dates
-    line_chart.add('Price', line_prices)
-    line_chart.add('Open', line_open_prices)
-    line_chart.add('High', line_high_prices)
-    line_chart.add('Low', line_low_prices)
-    line_chart.add('Close', line_close_prices)
-
-    # Create bar chart
-    bar_chart = pygal.Bar(x_label_rotation=20)
-    bar_chart.title = f'{stock_symbol} Stock Volume'
-    bar_chart.x_labels = bar_dates
-    bar_chart.add('Volume', bar_volumes)
-
-    # Render charts to SVG files
-    line_chart.render_to_file('line_chart.svg')
-    bar_chart.render_to_file('bar_chart.svg')
-
-# Generate HTML page with embedded charts
-    html_element = etree.Element('html')
-    head = etree.SubElement(html_element, 'head')
-    title = etree.SubElement(head, 'title')
-    title.text = 'Stock Charts'
-    body = etree.SubElement(html_element, 'body')
-
-    if chart_type == '1':
-        with open('bar_chart.svg', 'r') as f:
-            bar_svg_data = f.read()
-        bar_svg = etree.fromstring(bar_svg_data.encode())
-        body.append(bar_svg)
-
-    elif chart_type == '2':
-        with open('line_chart.svg', 'r') as f:
-            line_svg_data = f.read()
-        line_svg = etree.fromstring(line_svg_data.encode())
-        body.append(line_svg)
-
+   # pingAPI(time_series, stock_symbol, start_date, end_date)
+    
+    if time_series == 1:
+        url = f'https://www.alphavantage.co/query?function={time_series}&symbol=' + stock_symbol + '&outputsize=full&interval=60min&outputsize=full&apikey=GIW0WKEDYCA4NMCQ'
+        r = requests.get(url)
+        data = r.json()
+        inputRange = pandas.date_range(start_date, end_date, freq='H').strftime('%Y-%m-%d %h:%m:%s')
+    elif time_series == 2:
+        url = f'https://www.alphavantage.co/query?function={time_series}&symbol=' + stock_symbol + '&outputsize=full&apikey=GIW0WKEDYCA4NMCQ'
+        r = requests.get(url)
+        data = r.json()
+        inputRange = pandas.date_range(start_date, end_date, freq='D').strftime('%Y-%m-%d')
+    elif time_series == 3:
+        url = f'https://www.alphavantage.co/query?function={time_series}&symbol=' + stock_symbol + '&outputsize=full&apikey=GIW0WKEDYCA4NMCQ'
+        r = requests.get(url)
+        data = r.json()
+        inputRange = pandas.date_range(start_date, end_date, freq='D').strftime('%Y-%m-%d')
     else:
-        # Throw error
-        print("Invalid chart type selected.")
+        url = f'https://www.alphavantage.co/query?function={time_series}&symbol=' + stock_symbol + '&outputsize=full&apikey=GIW0WKEDYCA4NMCQ'
+        r = requests.get(url)
+        data = r.json()
+        inputRange = pandas.date_range(start_date, end_date, freq='D').strftime('%Y-%m-%d')
 
-    # Output HTML
-    with open('stock_charts.html', 'wb') as f:
-        f.write(html.tostring(html_element, pretty_print=True))
+    #dataSeperator calls
+    openData = dataSeperator("1. open")
+    highData = dataSeperator("2. high")
+    lowData = dataSeperator("3. low")
+    closeData = dataSeperator("4. close")
+    xAxisDate = dateChecker()
+    
+    if chart_type == 2:
+       line_chart = pygal.Line(x_label_rotation=45)
+       line_chart.title = 'Stock Data for ' + stock_symbol + ': ' + start_date + ' to ' + end_date
+       line_chart.x_labels = xAxisDate
+       line_chart.add('Open', openData)
+       line_chart.add('High', highData)
+       line_chart.add('Low',  lowData)
+       line_chart.add('Close', closeData)
+       line_chart.render_in_browser()
+    else:
+       bar_chart = pygal.Bar(x_label_rotation=45)
+       bar_chart.title = 'Stock Data for ' + stock_symbol + ': ' + start_date + ' ' + end_date
+       bar_chart.x_labels = xAxisDate
+       bar_chart.add('Open', openData)
+       bar_chart.add('High', highData)
+       bar_chart.add('Low',  lowData)
+       bar_chart.add('Close', closeData)
+       bar_chart.render_in_browser()
 
-    # Open HTML file in browser
-    html.open_in_browser(html_element)
-
+    #print(f"Data = {data}")
+        
 main()
